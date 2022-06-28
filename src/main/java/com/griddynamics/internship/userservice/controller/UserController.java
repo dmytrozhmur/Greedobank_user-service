e6217297ed3a2b99016a5a02fab200b173aff0f6
forAzur <dmytro.zhmur@nure.ua>
2022-06-28 15:15:46 +0300

    spring boot template

---
+++ src/main/java/com/griddynamics/internship/userservice/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.griddynamics.internship.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/v1/users")
+    public String getUserList() {
+        return "Empty user list";
+    }
+
+}
---
+++ src/test/java/com/griddynamics/internship/userservice/HttpRequestTest.java
@@ -0,0 +1,27 @@
+package com.griddynamics.internship.userservice;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getUserListReturnsDefaultMessage() {
+        assertThat(this.restTemplate.getForObject(String.format("http://localhost:%d/api/v1/users", port),
+                String.class)).contains("Empty user list");
+    }
+}
