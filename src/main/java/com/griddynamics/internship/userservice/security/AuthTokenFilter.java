b649f5eb42740b5f82144d8b1fb2c749786c445f
forAzur <dmytro.zhmur@nure.ua>
2022-07-30 14:00:34 +0300

    Added AuthTokenFilter.java

---
+++ src/main/java/com/griddynamics/internship/userservice/security/AuthTokenFilter.java
@@ -0,0 +1,54 @@
+package com.griddynamics.internship.userservice.security;
+
+import com.griddynamics.internship.userservice.exception.AuthenticationException;
+import com.griddynamics.internship.userservice.model.UserDetailsImpl;
+import com.griddynamics.internship.userservice.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = getTokenFromRequest(request);
+        if(token != null) {
+            try {
+                String email = JwtUtils.getEmail(token);
+
+                UserDetailsImpl userDetails = (UserDetailsImpl) userDetailsService.loadUserByUsername(email);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null
+                );
+                authenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            } catch (Exception e) {
+                throw new AuthenticationException("Cannot set user authentication");
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+
+        if(StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer "))
+            return authHeader.substring(7);
+        return null;
+    }
+}
